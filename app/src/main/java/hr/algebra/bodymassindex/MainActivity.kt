810945543ce30e571838126e7646f6a5fff33fd8
Petar Markota <pmarko1@racunarstvo.hr>
2024-10-23 18:11:17 +0200

    almost over

--- app/src/main/java/hr/algebra/bodymassindex/MainActivity.kt
+++ app/src/main/java/hr/algebra/bodymassindex/MainActivity.kt
@@ -1,20 +1,76 @@
 package hr.algebra.bodymassindex
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import hr.algebra.bodymassindex.databinding.ActivityMainBinding
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var validationFields: List<EditText>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initValidation()
+        setupListeners()
+    }
+
+    private fun initValidation() {
+        validationFields= listOf(
+            binding.etHeight,
+            binding.etWeight
+        )
+    }
+
+    private fun setupListeners() {
+        binding.btnCalculate.setOnClickListener {
+            if(formValid()) {
+                val result: Double? = calculate(
+                    binding.etWeight.text.toString().toDouble(),
+                    binding.etHeight.text.toString().toDouble()
+                )
+                showResult(result)
+            }
+        }
+    }
+
+    private fun formValid(): Boolean {
+        validationFields.forEach{
+            if (it.text.toString().isBlank()){
+                it.error= getString(R.string.please_insert_value)
+                it.requestFocus()
+                return false
+            }
+        }
+        return true
+
+    }
+
+    private fun calculate(weight: Double, height: Double): Double? {
+        if(weight <=0 || height <=0){
+            return null
+        }
+        else{
+            val heightFinal = if(height>3) height/100 else height
+            return weight/heightFinal.pow(2)
+        }
+    }
+
+    private fun showResult(result: Double?)=when {
+        result==null->{
+
+        }result<=20->{
+
+        }else->{
+
         }
     }
 }
